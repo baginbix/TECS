e51fdcf3e803dcc3dd134582650ecacfa6cb19fe
baginbix <timtuvestam@hotmail.se>
2026-03-10 15:27:55 +0100

    Delete Game directory

--- Game/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿
-using System;
-using System.Collections;
-using System.Collections.Generic;
-
-using var game = new TestGame.Game1();
-game.Run();
-
-//var summary = BenchmarkRunner.Run<PerformanceTest>();
