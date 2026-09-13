1ac4e2043a63e2913056d81c8dac3bba81d847d5
Baginbix <timtuvestam@hotmail.se>
2026-09-13 23:59:53 +0200

    Changed schedule phases to schedule labels for better flexibility

---
+++ src/TECS/Scheduler/StateSchedule.cs
@@ -0,0 +1,42 @@
+using TECS;
+using TECS.Commands;
+using TECS.Executors;
+using TECS.Scheduler;
+using TECS.Systems;
+
+class StateSchedule : IScheduler
+{
+    private List<IStateManager> _states = new();
+
+    public void AddSystem<TSchedule>(SystemBinding system) { }
+
+    public void AddManager(IStateManager manager)
+    {
+        if (!_states.Contains(manager))
+            _states.Add(manager);
+    }
+
+    public void RunPhase(ECS ecs)
+    {
+        var cmd = new CommandBuffer();
+        foreach (var state in _states)
+        {
+            state.ProcessTransitions(ecs, ref cmd);
+        }
+
+        cmd.Flush(ecs);
+    }
+
+    public void RunUpdate(ECS ecs, CommandBuffer cmd)
+    {
+        foreach (var state in _states)
+        {
+            state.RunActiveState(ecs, ref cmd);
+        }
+    }
+
+    public void SetExecutor(IExecutor executor)
+    {
+        throw new NotImplementedException();
+    }
+}
